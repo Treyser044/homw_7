82a289ee6fa595df25b481503603c313513868fc
Treyser044 <treyser044@gmail.com>
2025-07-07 20:01:00 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+/*let nameStudent = ['yunus', 'odil' , 'muhammad' , 'timur']
+
+nameStudent.push('Odil')
+nameStudent.push('behruz')
+nameStudent.push('guty')
+
+nameStudent.pop()
+nameStudent.unshift('kozim')
+
+nameStudent.push({tsudent:'yunus' , age:'22'})
+nameStudent.push('pop')
+console.log(nameStudent);
+*/
+let fruits = ['Olma', 'Banan', 'Gilos', 'Shaftoli']
+
+console.log(fruits);
+
+
+alert('Boshlangich arrayning uzunligi:4')
+
+let javob = confirm('sizning arrayingizdan malumotlarni olib tashlamoqchimiz')
+
+if (javob) {
+
+    fruits.pop()
+    alert('arrayning uzunligi:3')
+    
+}else(alert('arryning uzunligi:4'))
+    
+console.log(fruits);
+
+
